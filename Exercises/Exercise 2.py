687e50e1e0c9f231003bd78bf5c1cbef9ef993a0
SivuyileMtwetwe <sivuyilemtwetwe@gmail.com>
2024-10-22 10:23:20 +0200

    Structure

---
+++ Exercises/Exercise 2.py
@@ -0,0 +1,68 @@
+# Question 1: Arithmetic and Assignment Operators
+
+# TODO: Add 3 to x using the += operator
+x = 0
+x += 3
+# TODO: Multiply y by 2 using the *= operator
+y = 1
+y *=2
+# TODO: Divide x by y and store the result in a variable called 'result'
+result = x/y
+# TODO: Print the value of 'result'
+print(result)
+#------------------------------------------------------------------------------------
+# Question 2: Comparison and Logical Operators
+
+# TODO: Create a condition that checks if a is greater than b
+a = 0
+b = 0
+c = 0
+if a>b:
+     True
+else:
+     False
+# TODO: Create a condition that checks if b is even (hint: use the modulus operator)d
+if b %2 == 0:
+     True
+else:
+     False
+
+# TODO: Create a condition that checks if c is less than or equal to a
+if c <= a:
+     True
+else:
+     False
+# TODO: Combine the above conditions using logical operators to create a 'final_condition'
+#       The 'final_condition' should be True if either:
+#       - a is greater than b, or
+#       - b is even and c is less than or equal to a
+def final_condition():
+
+# TODO: Print the value of 'final_condition'
+    print(final_condition)
+#------------------------------------------------------------------------------------
+# Question 3: Conditional Statements
+
+# TODO: Ask the user to input a test score (0-100) and store it in a variable called 'score'
+
+# TODO: Implement a grading system using if-elif-else statements:
+#       90-100: A
+#       80-89: B
+#       70-79: C
+#       60-69: D
+#       Below 60: F
+
+# TODO: Print the grade for the given score
+
+#------------------------------------------------------------------------------------
+# Question 4: Combining Operators and Conditionals
+
+# TODO: Ask the user to input two numbers and store them in variables 'num1' and 'num2'
+
+# TODO: Ask the user to input an operation (+, -, *, /) and store it in a variable called 'operation'
+
+# TODO: Use conditional statements to perform the chosen operation on num1 and num2
+# TODO: Handle the case of division by zero
+
+
+# TODO: Print the result of the operation
