8695862553a657d92babcdc854bf1191edd60809
SivuyileMtwetwe <sivuyilemtwetwe@gmail.com>
2024-10-24 12:02:04 +0200

    in progress

--- Exercise 4 answer.py
+++ Exercise 4 answer.py
@@ -3,29 +3,35 @@
 # TODO: Create a list of fruits
 fruits = ["apple", "orange", "banana", "grape", "lemon","mango","strawberry", "naartjies"]
 # TODO: Use a for loop to print each fruit in the list
-for i in fruits:
-    print(i)
+# for i in fruits:
+    # print(i)
 
 #-------------------------------------------------------------------------
 # Question 2: Using a while loop for countdown
 
 # TODO: Use a while loop to create a countdown from 5 to 1
+# j = 6
 
+# while j>1:
+#     j-=1
+#     print(j)
+    
 
 #-------------------------------------------------------------------------
 # Question 3: Using a for loop with range()
 
 # TODO: Use a for loop to print the first 10 square numbers
-
-
+# for i in range(1,11):
+#     print(i**2)
 #-------------------------------------------------------------------------
 
 # Question 4: Using the random module
 
 # TODO: Import the random module
-
+import random
 # TODO: Create a list of colors
-
+colours = [random.random]
+print(colours)
 # TODO: Use a for loop to select and print 3 random colors from the list
 
 
