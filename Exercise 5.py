75a5a25e6a8be8c97cfe7235e453d7c48c53a380
SivuyileMtwetwe <sivuyilemtwetwe@gmail.com>
2024-10-25 11:08:19 +0200

    in progress

--- Exercise 5.py
+++ Exercise 5.py
@@ -1,27 +1,37 @@
-# Question 1: Creating and Modifying Lists
+# # Question 1: Creating and Modifying Lists
 
-# TODO: Create a list of fruits
+# # TODO: Create a list of fruits
+# fruits = ["apple", "orange", "banana", "grape", "lemon","mango","strawberry", "naartjies"]
+# # TODO: Add a fruit to the end of the list
+# fruits.append("peach")
 
-# TODO: Add a fruit to the end of the list
-
-# TODO: Insert a fruit at the beginning of the list
-
-# TODO: Remove a fruit from the list
-
-# TODO: Print the modified list
-
+# # TODO: Insert a fruit at the beginning of the list
+# fruits.insert(0,"pear")
+# # TODO: Remove a fruit from the list
+# fruits.remove("grape")
+# # TODO: Print the modified list
+# print(fruits)
+ 
 
 #-------------------------------------------------------------------------
 # Question 2: List Operations
 
 # TODO: Create a list of numbers from 1 to 5
-
+numbers = [1,2,3,4,5]
 # TODO: Create a new list with each number squared
+new_list = []
 
+for num in numbers:
+    new_list.append(num**2)
+
+print(new_list)
 # TODO: Find the sum and average of the original numbers
-
+sum_ = sum(numbers)
+average = sum(numbers)/len(numbers)
 # TODO: Print the results
+print(sum_)
 
+print(average) 
 
 #-------------------------------------------------------------------------
 # Question 3: Creating and Modifying Dictionaries
